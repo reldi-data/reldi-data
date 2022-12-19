ec5b994f42bd77d3aa61d9bac35cb21eab24a060
Vanja Štefanec <vanja.stefanec@broj42.com>
2022-12-19 02:34:42 +0100

    Modifying .conllu generating script to support sentence-level containment

--- generate_conllu.py
+++ generate_conllu.py
@@ -3,6 +3,7 @@ import os
 import re
 
 from collections import namedtuple, OrderedDict
+from enum import Enum
 from io import StringIO
 
 
@@ -10,6 +11,7 @@ DATASETS_RE = re.compile(r'(?<=(?:# contained_in_datasets = ))(.+)(?=\n)')
 ANNOTATIONS_RE = re.compile(r'(?<=(?:# annotation_levels = ))(.+)(?=\n)')
 
 CORPUS_NULL_VALUES = ['_', '*']
+ReadingMode = Enum('ReadingMode', ['DOCUMENT', 'SENTENCE'])
 
 
 class CONLLUPToken(namedtuple('CONLLUPToken', ['index', 'form', 'lemma', 'upos', 'msd', 'upos_feats', 'head', 'deprel',
@@ -58,73 +60,184 @@ class CONLLUToken(namedtuple('CONLLUToken', ['index', 'form', 'lemma', 'upos', '
 def generate(input_stream, output_stream, datasets=[], omit_datasets=[], annotations=[], omit_annotations=[],
              misc=[], keep_status=False):
 
-    flush = False
+    def consume_till_end(*end_marks, flush=False):
+        while True:
+            line = input_stream.readline()
+
+            if not line or any([line.startswith(end_mark) for end_mark in end_marks]):
+                break
+
+            if flush:
+                continue
+
+            if line.startswith('# contained_in_datasets') or line.startswith('# annotation_levels'):
+                if keep_status:
+                    output_stream.write(line)
+
+            elif line.startswith('# ') or not line.strip():
+                output_stream.write(line)
+
+            else:
+                output_stream.write(
+                    CONLLUToken.create_from_conllup_token(
+                        CONLLUPToken.create_from_conllup_line(line.strip()),
+                        misc
+                    ).to_conllu_line()
+                )
+
+        return line
+
     read_buffer = StringIO()
 
-    for line in input_stream:
-        if line.startswith('# global.columns') or (flush and not line.startswith('# newdoc')):
+    reading_mode = ReadingMode.DOCUMENT
+
+    line = input_stream.readline()
+
+    while True:
+
+        if not line:
+            break
+
+        if line.startswith('# global.columns'):
+            line = input_stream.readline()
             continue
 
         if line.startswith('# newdoc'):
-            flush = False
+            reading_mode = ReadingMode.DOCUMENT
             read_buffer = StringIO()
             if any([datasets, omit_datasets, annotations, omit_annotations]):
+                # there are limitations, buffer and wait
                 read_buffer.write(line)
             else:
+                # no limitations, write till the end
                 output_stream.write(line)
+                line = consume_till_end('# newdoc')
+                continue
+
+        elif line.startswith('# sent_id'):
+            read_buffer = StringIO()
+            if reading_mode == ReadingMode.SENTENCE and any([datasets, omit_datasets]):
+                # there are limitations, buffer and wait
+                read_buffer.write(line)
+            else:
+                # no limitations, write till the end
+                line = consume_till_end('# sent_id', '# newdoc')
+                continue
 
         elif line.startswith('# contained_in_datasets'):
-            if datasets or omit_datasets:
-                doc_datasets = DATASETS_RE.search(line).group(0).split(';')
-                if omit_datasets and set(doc_datasets).intersection(omit_datasets):
-                    flush = True
-                    continue
-                if datasets and not set(doc_datasets).intersection(datasets):
-                    flush = True
-                    continue
-                if annotations or omit_annotations:
+            if reading_mode == ReadingMode.DOCUMENT:
+                if datasets or omit_datasets:
+                    # there are dataset-related limitations
+                    doc_datasets = DATASETS_RE.search(line).group(0).split(';')
+                    if omit_datasets and set([d.strip('*') for d in doc_datasets]).intersection(omit_datasets):
+                        # document is part of the omited dataset(s), reset buffer and flush
+                        read_buffer = StringIO()
+                        line = consume_till_end('# newdoc', flush=True)
+                        continue
+                    if datasets and not set([d.strip('*') for d in doc_datasets]).intersection(datasets):
+                        # document is not part of the required dataset(s), reset buffer and flush
+                        read_buffer = StringIO()
+                        line = consume_till_end('# newdoc', flush=True)
+                        continue
+
+                    if datasets and any([d.endswith('*') for d in doc_datasets]):
+                        # document is partially in different datasets, switch to sentence mode
+                        reading_mode = ReadingMode.SENTENCE
+
+                    if annotations or omit_annotations:
+                        # there are annotations-related limitations, buffer and wait
+                        if keep_status:
+                            read_buffer.write(line)
+                    else:
+                        # there are no annotations-related limitations, output buffered content and reset buffer
+                        output_stream.write(read_buffer.getvalue())
+                        read_buffer = StringIO()
+                        if keep_status:
+                            output_stream.write(line)
+
+                        if reading_mode == ReadingMode.DOCUMENT:
+                            # no limitations and in document mode, write till the end
+                            line = consume_till_end('# newdoc')
+                            continue
+
+                elif annotations or omit_annotations:
+                    # there are annotations-related limitations, buffet and wait
                     if keep_status:
                         read_buffer.write(line)
                 else:
+                    # there are no limitations, output buffered content and reset buffer
                     output_stream.write(read_buffer.getvalue())
                     read_buffer = StringIO()
                     if keep_status:
                         output_stream.write(line)
-            else:
+
+                    if reading_mode == ReadingMode.DOCUMENT:
+                        # no limitations and in document mode, write till the end
+                        line = consume_till_end('# newdoc')
+                        continue
+
+            elif reading_mode == ReadingMode.SENTENCE:
+                if datasets or omit_datasets:
+                    # there are dataset-related limitations
+                    doc_datasets = DATASETS_RE.search(line).group(0).split(';')
+                    if omit_datasets and set(doc_datasets).intersection(omit_datasets):
+                        # sentence is part of the omited dataset(s), reset buffer and flush
+                        line = consume_till_end('# sent_id', '# newdoc', flush=True)
+                        continue
+                    if datasets and not set(doc_datasets).intersection(datasets):
+                        # sentence is not part of the required dataset(s), reset buffer and flush
+                        line = consume_till_end('# sent_id', '# newdoc', flush=True)
+                        continue
+
+                # pass all limitations, output buffered content and reset buffer
                 output_stream.write(read_buffer.getvalue())
                 read_buffer = StringIO()
                 if keep_status:
                     output_stream.write(line)
 
+                # write till the end
+                line = consume_till_end('# sent_id', '# newdoc')
+                continue
+
         elif line.startswith('# annotation_levels'):
             if annotations or omit_annotations:
+                # there are annotations-related limitations
                 doc_annotations = ANNOTATIONS_RE.search(line).group(0).split(';')
                 if omit_annotations and any([a in doc_annotations for a in omit_annotations]):
-                    flush = True
+                    # document contains omited annotation(s), reset buffer and flush
+                    read_buffer = StringIO()
+                    line = consume_till_end('# newdoc', flush=True)
                     continue
                 if annotations and not all([a in doc_annotations for a in annotations]):
-                    flush = True
+                    # document has no required annotation(s), reset buffer and flush
+                    read_buffer = StringIO()
+                    line = consume_till_end('# newdoc', flush=True)
                     continue
-                output_stream.write(read_buffer.getvalue())
-                read_buffer = StringIO()
-                if keep_status:
-                    output_stream.write(line)
-            else:
-                output_stream.write(read_buffer.getvalue())
-                read_buffer = StringIO()
-                if keep_status:
-                    output_stream.write(line)
+
+            # pass all limitations, output buffered content and reset buffer
+            output_stream.write(read_buffer.getvalue())
+            read_buffer = StringIO()
+            if keep_status:
+                output_stream.write(line)
+
+            if reading_mode == ReadingMode.DOCUMENT:
+                # in document mode, write till the end
+                line = consume_till_end('# newdoc')
+                continue
 
         elif line.startswith('# ') or not line.strip():
             output_stream.write(line)
 
         else:
+            # shouldn't end up here
             output_stream.write(
                 CONLLUToken.create_from_conllup_token(
                     CONLLUPToken.create_from_conllup_line(line.strip()),
                     misc
                 ).to_conllu_line()
             )
+
+        line = input_stream.readline()
 
 
 def main(args):
