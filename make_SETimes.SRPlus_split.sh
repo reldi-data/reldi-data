e9904200797bf7951a28bc78625a9b8548edc39c
Vanja Štefanec <vanja.stefanec@broj42.com>
2022-12-27 15:44:56 +0100

    Correcting standard corpus splitting scripts, transferring extra columns to misc

--- make_SETimes.SRPlus_split.sh
+++ make_SETimes.SRPlus_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-train.conllu -d set.sr.plus-train
-python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-dev.conllu -d set.sr.plus-dev
-python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-test.conllu -d set.sr.plus-test
+python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-train.conllu -d set.sr.plus-train -m NE
+python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-dev.conllu -d set.sr.plus-dev -m NE
+python3 generate_conllu.py SETimes.SRPlus/set.sr.plus.conllup -o SETimes.SRPlus/set.sr.plus-test.conllu -d set.sr.plus-test -m NE
--- make_hr500k_split.sh
+++ make_hr500k_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-train.conllu -d hr500k-train
-python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-dev.conllu -d hr500k-dev
-python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-test.conllu -d hr500k-test
+python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-train.conllu -d hr500k-train -m NE DP SRL
+python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-dev.conllu -d hr500k-dev -m NE DP SRL
+python3 generate_conllu.py hr500k/hr500k.conllup -o hr500k/hr500k-test.conllu -d hr500k-test -m NE DP SRL
